ed59b7b9cf39f51787085cc51d457ebffb3595f4
Marek Kotowski <marek.kotowski87@gmail.com>
2018-07-09 22:48:38 +0200

    Project finish - smooth scrolling

--- LoopLabSocialTheme/wwwroot/js/script.js
+++ LoopLabSocialTheme/wwwroot/js/script.js
@@ -1,2 +1,20 @@
 ﻿//Get the current year for copyright
 $('#year').text(new Date().getFullYear());
+
+//Init scrollspy
+$('body').scrollspy({ target: '#main-nav' });
+
+//Smooth scrolling
+$("#main-nav a").on('click', function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function () {
+            window.location.hash = hash;
+        });
+    }
+});
